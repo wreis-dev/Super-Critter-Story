c05116197a91fd54d07de9266d8b849e52fdf02e
Wagner Reis <wreis.dev@gmail.com>
2026-05-01 11:35:06 -0300

    feat: add survey page for wishlist validation

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
   base: "/Super-Critter-Story/",
   build: {
     assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL("./index.html", import.meta.url)),
+        pesquisa: fileURLToPath(new URL("./pesquisa.html", import.meta.url)),
+      },
+    },
   },
 });
